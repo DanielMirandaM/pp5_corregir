617a271632b8c4b0347e751e0d7b17c5cb35e26c
DanielMirandaM <88871271+DanielMirandaM@users.noreply.github.com>
2021-08-12 23:01:24 -0600

    Update ProyectoProgramado4Corregido.c

--- ProyectoProgramado4Corregido.c
+++ ProyectoProgramado4Corregido.c
@@ -7,20 +7,20 @@ using namespace std ;
 
 int main(){
 
- int n,aux;
-    cout<<"Ingrese la cantidad de numeros :";
-    cin>>n;
+ int n,aux;//1 error no estaba el ; al final de la línea
+    cout<<"Ingrese la cantidad de numeros :"; //Se le solicita al usuario la cantidad de numeros a ordenar
+    cin>>n; //se guarda la cantidad en la variable n
  int V[n];
  for(int i=0;i<=n;i++){
   cout<<"\n Numero "<<i+1<<" = ";
-  cin>>V[i];
+  cin>>V[i]; //2 error faltaba el ; después de almacenar el valor en i
  }
 
- for(int i=0;i<=n;i++){
+ for(int i=0;i<=n;i++){ //Error 3 faltaba + en la inicialización
   for(int j=i+1;j<n;j++){
    if(V[j]<V[i]){
-    aux=V[i];
-    V[i]=V[j];
+    aux=V[i]; //Hacía falta la i dentro
+    V[i]=V[j]; // Hacía falta la j dentro
     V[j]=aux;
     
    }
